6531e3356d8ee0c5876bb1431ab75cbbeabd56a8
Kolyanzz <guslyakov3343@gmail.com>
2021-01-12 22:44:53 +0900

    Пункты 1 и 2

--- app/src/main/java/com/example/pz1/MainActivity.java
+++ app/src/main/java/com/example/pz1/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.pz1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button1);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TwoActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
